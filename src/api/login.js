3e08cb617f30610f8e44f1b21e2497352b34126d
李铭昕 <715557344@qq.com>
2018-01-19 11:24:12 +0800

    修改代码规范

--- src/api/login.js
+++ src/api/login.js
@@ -1,28 +1,28 @@
 import request from '@/utils/request'
 
 export function login(username, password) {
-    return request({
-        url: '/api/user/login',
-        method: 'post',
-        params: {
-            username,
-            password
-        }
-    })
+  return request({
+    url: '/api/user/login',
+    method: 'post',
+    params: {
+      username,
+      password
+    }
+  })
 }
 
 export function getInfo(token) {
-    return request({
-        url: '/api/user/info',
-        method: 'post',
-        params: {token}
-    })
+  return request({
+    url: '/api/user/info',
+    method: 'post',
+    params: { token }
+  })
 }
 
 export function logout(token) {
-    return request({
-        url: '/api/user/logout',
-        method: 'post',
-        params: {token}
-    })
+  return request({
+    url: '/api/user/logout',
+    method: 'post',
+    params: { token }
+  })
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -32,64 +32,64 @@
 </template>
 
 <script>
-    import {isvalidUsername} from '@/utils/validate'
+  import { isvalidUsername } from '@/utils/validate'
 
-    export default {
-        name: 'login',
-        data() {
-            const validateUsername = (rule, value, callback) => {
-                if (!isvalidUsername(value)) {
-                    callback(new Error('请输入正确的用户名'))
-                } else {
-                    callback()
-                }
-            }
-            const validatePass = (rule, value, callback) => {
-                if (value.length < 5) {
-                    callback(new Error('密码不能小于5位'))
-                } else {
-                    callback()
-                }
-            }
-            return {
-                loginForm: {
-                    username: 'superadmin',
-                    password: 'superadmin'
-                },
-                loginRules: {
-                    username: [{required: true, trigger: 'blur', validator: validateUsername}],
-                    password: [{required: true, trigger: 'blur', validator: validatePass}]
-                },
-                loading: false,
-                pwdType: 'password'
-            }
-        },
-        methods: {
-            showPwd() {
-                if (this.pwdType === 'password') {
-                    this.pwdType = ''
-                } else {
-                    this.pwdType = 'password'
-                }
-            },
-            handleLogin() {
-                this.$refs.loginForm.validate(valid => {
-                    if (valid) {
-                        this.loading = true
-                        this.$store.dispatch('Login', this.loginForm).then(() => {
-                            this.loading = false
-                            this.$router.push({path: '/'})
-                        }).catch(() => {
-                            this.loading = false
-                        })
-                    } else {
-                        console.log('error submit!!')
-                        return false
-                    }
-                })
-            }
+  export default {
+    name: 'login',
+    data() {
+      const validateUsername = (rule, value, callback) => {
+        if (!isvalidUsername(value)) {
+          callback(new Error('请输入正确的用户名'))
+        } else {
+          callback()
         }
+      }
+      const validatePass = (rule, value, callback) => {
+        if (value.length < 5) {
+          callback(new Error('密码不能小于5位'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        loginForm: {
+          username: 'superadmin',
+          password: 'superadmin'
+        },
+        loginRules: {
+          username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+          password: [{ required: true, trigger: 'blur', validator: validatePass }]
+        },
+        loading: false,
+        pwdType: 'password'
+      }
+    },
+    methods: {
+      showPwd() {
+        if (this.pwdType === 'password') {
+          this.pwdType = ''
+        } else {
+          this.pwdType = 'password'
+        }
+      },
+      handleLogin() {
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            this.loading = true
+            this.$store.dispatch('Login', this.loginForm).then(() => {
+              this.loading = false
+              this.$router.push({ path: '/' })
+            }).catch(() => {
+              this.loading = false
+            })
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      }
     }
+  }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
