bdc4f9d9dcc9169d79db254a931aa2b2b6c3abc4
李铭昕 <715557344@qq.com>
2018-01-22 12:46:20 +0800

    增加角色

--- src/views/role/role_add.vue
+++ src/views/role/role_add.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="app-container">
-        <el-form ref="route_add" :model="form" label-width="120px">
-            <el-form-item label="路由名字">
-                <el-input v-model="form.name"></el-input>
+        <el-form ref="role_add" :model="form" label-width="120px">
+            <el-form-item label="角色名">
+                <el-input v-model="form.roleName"></el-input>
             </el-form-item>
-            <el-form-item label="路由规则">
-                <el-input v-model="form.route"></el-input>
+            <el-form-item label="角色介绍">
+                <el-input v-model="form.roleDesc"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="onSubmit">保存</el-button>
@@ -16,20 +16,20 @@
 </template>
 
 <script>
-  import { addRoute } from '@/api/router'
+  import { addRole } from '@/api/role'
 
   export default {
     data() {
       return {
         form: {
-          name: '',
-          route: ''
+          roleName: '',
+          roleDesc: ''
         }
       }
     },
     methods: {
       onSubmit() {
-        addRoute(this.form).then(response => {
+        addRole(this.form).then(response => {
           const data = response.data
           console.log(data)
           this.$message('保存成功')
