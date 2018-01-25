6f392cb7d28f94356a5a2ca05418d8eaf14760f1
李铭昕 <715557344@qq.com>
2018-01-25 15:58:52 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://api.service.thrift.phalcon.xx/"',
+  BASE_API: '"http://api.service.phalcon.xx/"',
 })
